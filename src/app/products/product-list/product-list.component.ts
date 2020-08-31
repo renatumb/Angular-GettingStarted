abd545e4b8e33b24f1d97290f8b6665cd12eda74
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-08-30 23:20:09 -0700

    004.  > Property Binding, Event Binding and Two way Binding with ngModel  > Pipe

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -7,6 +7,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean;
+  listFilter01 : string  = 'FilterInitial';
   products: any[] = [
     {
       'productId': 1,
@@ -66,4 +70,7 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
 }
