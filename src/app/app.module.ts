9f768939a5f07dbeaf1917c126b0f98f86634043
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-09-05 16:34:38 -0700

    015. > Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,14 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
 import {FormsModule} from '@angular/forms';
-import { ConvertToSpacePipe } from './utils/pipes/convert-to-space.pipe';
-import { StarComponent } from './shared/star/star.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
 import {RouterModule} from '@angular/router';
-import {ProductDetailGuard} from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacePipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
@@ -26,12 +18,11 @@ import {ProductDetailGuard} from './products/product-detail.guard';
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]},
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
