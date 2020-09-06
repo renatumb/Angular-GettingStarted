882405bd613c51da00e713a7cd8bde518065c458
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-09-05 17:04:18 -0700

    016. > structured of the app and last module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,28 +1,22 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {ProductListComponent} from './product-list/product-list.component';
-import {ConvertToSpacePipe} from '../utils/pipes/convert-to-space.pipe';
-import {StarComponent} from '../shared/star/star.component';
 import {ProductDetailComponent} from './product-detail/product-detail.component';
-import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {ProductDetailGuard} from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ConvertToSpacePipe,
-    StarComponent,
     ProductDetailComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]}
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule {
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {StarComponent} from './star/star.component';
+import {FormsModule} from '@angular/forms';
+import {ConvertToSpacePipe} from '../utils/pipes/convert-to-space.pipe';
+
+
+
+@NgModule({
+  declarations: [ StarComponent,
+    ConvertToSpacePipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    StarComponent,
+    ConvertToSpacePipe,
+    CommonModule,
+    FormsModule
+  ]
+})
+export class SharedModule { }
