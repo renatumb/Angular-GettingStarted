563a722a95bb582173e05d95b1cd819b6d43e219
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-09-20 15:09:31 -0700

    019. > Adding routing to product-edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,12 @@ import {FormsModule} from '@angular/forms';
 import { WelcomeComponent } from './home/welcome/welcome.component';
 import {RouterModule} from '@angular/router';
 import { ProductModule } from './products/product.module';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductEditComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {ProductListComponent} from './product-list/product-list.component';
 import {ProductDetailComponent} from './product-detail/product-detail.component';
+
 import {RouterModule} from '@angular/router';
 import {ProductDetailGuard} from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
+import {ProductEditComponent} from './product-edit/product-edit.component';
+
 
 
 @NgModule({
@@ -14,7 +17,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]}
+      {path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard]},
+      {path: 'products/:id/edit', component: ProductEditComponent}
     ]),
     SharedModule
   ]
